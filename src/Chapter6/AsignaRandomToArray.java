059cf344dab74303c992b54eaeabbfe59aa6b268
Usuario <Usuario@192.168.0.5>
2015-06-05 16:00:31 -0600

    new commit.2

--- src/Chapter6/AsignaRandomToArray.java
+++ src/Chapter6/AsignaRandomToArray.java
@@ -4,17 +4,29 @@
  * and open the template in the editor.
  */
 package Chapter6;
-
+import java.util.ArrayList;
+import Chapter6.ParejaDeCoordenadas;
+import Chapter6.RandomNumber;
 /**
  *
  * @author Usuario
  */
 public class AsignaRandomToArray {
     
-    public double Asigna (double []x){
-        double resultado = 0.0;
+    public ArrayList  Asigna (double []x){
+       ArrayList<ParejaDeCoordenadas> valores = new ArrayList<ParejaDeCoordenadas>();
+       
+       int largo = x.length;
+       RandomNumber number = new RandomNumber();
+       ParejaDeCoordenadas pareja = new ParejaDeCoordenadas();
+       
+       while(largo > 0){
+       pareja.setX(x[largo -1]);
+       pareja.setY(number.AleatorioEntre0y1());
+       largo -- ;  
+       }
+        return valores ;        
         
-        return resultado;
     }
     
 }
