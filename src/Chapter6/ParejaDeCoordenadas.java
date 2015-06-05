35c0d638247752301d35b54e6f2af55ea11d15a6
Usuario <Usuario@192.168.0.5>
2015-06-05 15:56:54 -0600

    new commit.1

---
+++ src/Chapter6/ParejaDeCoordenadas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ParejaDeCoordenadas {
+     double x = 0.0;
+     double y = 0.0;
+
+    public ParejaDeCoordenadas(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public ParejaDeCoordenadas()
+    {
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+
+}
+    
+
